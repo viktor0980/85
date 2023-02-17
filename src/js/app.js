472c390a5a8b8eb8d41ebe89542340234a454655
viktor0980 <viktor0980@gmail.com>
2023-02-17 16:01:01 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,12 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
+  
+const pokemons = async () => {
+
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=10");
+  const data = await response.json();
+  console.log(data);
+ };
+ pokemons();
 });
